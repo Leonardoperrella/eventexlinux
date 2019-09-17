db3ad96574ccbb978892751a633398ee41a2c234
Leonardo Augusto Perrella <leonardo.perrella@yahoo.com.br>
2019-09-17 10:12:20 -0300

    adiciona pago

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -24,6 +24,11 @@ class SubscriptionModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Henrique Bastos', str(self.obj))
 
+    def test_paid_default_to_False(self):
+        """by default paid must be false"""
+        self.assertEqual(False, self.obj.paid)
+
+
 
 
 
