f2ad3910f3c7e640d664063f9bc911323073d6d3
Leonardo Augusto Perrella <leonardo.perrella@yahoo.com.br>
2019-09-27 10:24:55 -0300

    contato

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from eventex.core.models import Speaker, Contact
 
-# Register your models here.
+
+#Utilizar o contat atraves do Speaker
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
+
+
+class SpeakerModelAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]  # Classes para o inline.
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'photo_img', 'website_link' ]
+
+    def website_link(self, obj):
+        return format_html('<a href={0}">{0}</a>', obj.website)
+
+    website_link.short_description = 'website'
+
+    def photo_img(self, obj):
+        return format_html('<img width="32px" src="{}" />', obj.photo)
+
+    photo_img.short_description = 'Foto'
+
+
+admin.site.register(Speaker, SpeakerModelAdmin)
+
+
--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.shortcuts import resolve_url as r
 
-# Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField('nome', max_length=255)
+    slug = models.SlugField('slug')
+    photo = models.URLField('foto')
+    website = models.URLField('website', blank=True)
+    description = models.TextField('descrição', blank=True)
+
+    class Meta:
+        verbose_name = 'palestrante'
+        verbose_name_plural = 'palestrantes'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return r('speaker_detail', slug=self.slug)
+
+
+class Contact(models.Model):
+    EMAIL = 'E'
+    PHONE = 'P'
+    KINDS = ((EMAIL, 'Email'), (PHONE, 'Telefone')) #Atrituto de classe
+    speaker = models.ForeignKey('Speaker', on_delete=models.CASCADE, verbose_name='palastrante')
+    kind = models.CharField('tipo', max_length=1, choices=KINDS)
+    value = models.CharField('valor', max_length=255)
+
+    class Meta:
+        verbose_name = 'contato'
+        verbose_name_plural = 'contatos'
+
+    def __str__(self):
+        return self.value
