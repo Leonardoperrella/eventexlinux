561527805985a40e95fd3e91c35a1a33d6a8afc7
Leonardo Augusto Perrella <leonardo.perrella@yahoo.com.br>
2019-10-08 10:13:32 -0300

    course como mti

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -40,6 +40,10 @@ class TalkMoldelTest(TestCase):
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
 
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
+
+
 
 class PeriodManagerTest(TestCase):
     def setUp(self):
@@ -86,3 +90,6 @@ class CourseModelTest(TestCase):
 
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
